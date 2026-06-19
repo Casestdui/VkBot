233719e055d59c68c4e972dd4ad2fc0fbd056b7b
Casestdui <ibocarov@vk.com>
2026-06-20 01:58:56 +0300

    Update пеленку

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using VkNet.Model;
 
 const long GroupId = 239555205;
 
-const string Token = "vk1.a.vQyiAcS08_gmRAuQ_3rweRvbAjNruoMcN9qryALdJyEfs5QI_9QeLZLXuVVgG7aciiuOOlyPcY-s5RfddtlRsUDiqXOwrl9Z1uCsSdK3fAEednj5y_PjkpYaTifUdZbjKQuEo9wtox1i66JSu3eWa_NAkUqANP5W79I-nxtvu_kTmmEbKgYzHAEakaWbIB8FEA3P6RiULrGLT1sLGlU1nw";
+const string Token = "Токен";
 
 var api = new VkApi();
 
